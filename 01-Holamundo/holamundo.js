5d6e830701860dd0ca4e8fd18ab345b564c36191
Laura <sofiabarreramontenegro26@gmail.com>
2025-09-02 20:12:19 -0500

    Proyecto JS

---
+++ 01-Holamundo/holamundo.js
@@ -0,0 +1,35 @@
+console.log('Hola mundo');
+
+let cajaDeAndy = "Woody";
+
+console.log(cajaDeAndy);
+
+let edad = 18;
+let nombre = "Laura";
+
+console.log('Mi nombre es '+nombre+" y tengo "+edad+" años ")
+
+//array
+
+let frutas = ['Manzana', 'Banano', 'Mango']
+
+console.log (frutas);
+
+console.log (frutas [1]);
+
+let frase = "JavaScript es extremadamente genial"
+
+
+console.log (frase);
+console.log (frase.length);
+console.log (frase.toLocaleUpperCase());
+console.log (frase.toLocaleLowerCase());
+
+//operaciones matematicas
+const suma = 3 + 12;
+
+console.log(suma);
+
+
+
+
